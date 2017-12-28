f242618ae5cbd100daf5dd7b47a2c5b09cdc4638
east4ming <cuikaidong@foxmail.com>
2017-12-28 20:07:09 +0800

    20171226: function

---
+++ 8_func/8_6_city.py
@@ -0,0 +1,9 @@
+"""City Country.
+
+接受城市的名称及其所属的国家. 并返回.
+"""
+
+def city_country(city_name, country):
+    """City and Country."""
+    city_info = city_name + ' ' + country
+    return city_info.title()
